5f59c5a272dc9d4b884bbad3fa2f8e829bfe2429
rodrigolive <rodrigolive@gmail.com>
2009-09-13 18:04:12 +0200

    first commit

---
+++ cpan_sh.js
@@ -0,0 +1,24 @@
+javascript:(function() {
+	var pre = document.body.children[0];
+	var code = pre.innerHTML;
+
+	document.body.innerHTML = '';
+
+	document.write('
+		<script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/2.0.320/scripts/shCore.js"><\/script>
+		<script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/2.0.320/scripts/shBrushPerl.js"><\/script>
+
+		<link type="text/css" rel="stylesheet" href="http://alexgorbatchev.com/pub/sh/2.0.320/styles/shCore.css"/>
+		<link type="text/css" rel="stylesheet" href="http://alexgorbatchev.com/pub/sh/2.0.320/styles/shThemeMidnight.css" id="shTheme" />
+
+		<style>
+			body { background-color: black; }
+		</style>
+		<script type="text/javascript">
+			SyntaxHighlighter.all();
+		<\/script>
+
+		<pre class="brush: perl">' + code + '<\/pre>
+	');
+	document.close();
+})()
