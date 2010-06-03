d172d33eb8378dc47324e83568124a82bc1dddf6
Rodrigo <calunia@gmail.com>
2010-06-03 14:07:57 -0700

    chrome, safari and firefox

--- cpan_shjs.js
+++ cpan_shjs.js
@@ -5,7 +5,7 @@ javascript:(function() {
 	code = code.replace( /(([A-Z]\w+::[A-Z]\w+)(::[A-Z]\w+)*)/g, '<a class="perldoc" href="http://search.cpan.org/perldoc?$1">$1</a>' );
 	code = code.replace( /^(use|require) (\w+(::\w+)*)/gm, '$1 <a class="perldoc" href="http://search.cpan.org/perldoc?$2">$2</a>' );
 
-	document.body.innerHTML = '
+	var html = '
 		<link type="text/css" rel="stylesheet" href="http://shjs.sourceforge.net/sh_style.css" />
 		<style>
 			.perldoc { color: brown; text-decoration: none; font-weight: bold; }
@@ -15,16 +15,31 @@ javascript:(function() {
 			/* sh_main.css overwrites */
 			pre.sh_sourceCode .sh_comment { color: #808080; font-style: italic; }
 		</style>
-
 		<pre class="sh_perl">' + code + '<\/pre>
+	';
+	var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+	if( is_firefox ) {
+		html = '
+			<script type="text/javascript" src="http://shjs.sourceforge.net/sh_main.min.js"></script>
+			<script type="text/javascript" src="http://shjs.sourceforge.net/lang/sh_perl.js"></script>
+		' + html + '
+			<script type="text/javascript">
+				sh_highlightDocument();
+			<\/script>
 		';
-	var sc=document.createElement('script');
-	sc.type = 'text/javascript';
-	sc.src = "http://shjs.sourceforge.net/lang/sh_perl.js"; 
-	document.body.appendChild(sc);
-	var sc2=document.createElement('script');
-	sc2.type = 'text/javascript';
-	sc2.src = "http://shjs.sourceforge.net/sh_main.min.js";
-	sc2.onload = function(){ sh_highlightDocument(); };
-	document.body.appendChild(sc2);
+		document.body.innerHTML = '';
+		document.write(html);
+		document.close();
+	} else {
+		document.body.innerHTML = html;
+		var sc=document.createElement('script');
+		sc.type = 'text/javascript';
+		sc.src = "http://shjs.sourceforge.net/lang/sh_perl.js"; 
+		document.body.appendChild(sc);
+		var sc2=document.createElement('script');
+		sc2.type = 'text/javascript';
+		sc2.src = "http://shjs.sourceforge.net/sh_main.min.js";
+		sc2.onload = function(){ sh_highlightDocument(); };
+		document.body.appendChild(sc2);
+	}
 })(); 
